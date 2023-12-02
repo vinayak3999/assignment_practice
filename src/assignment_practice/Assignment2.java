fd200a3d6d696012fb0eb82b479de43ef3d68547
lentraadmin <lentraadmin@lentraadmin-ThinkPad-E14-Gen-4>
2023-12-02 13:57:03 +0530

    Changes

--- src/assignment_practice/Assignment2.java
+++ src/assignment_practice/Assignment2.java
@@ -4,13 +4,13 @@ public class Assignment2 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter Integer number: ");
-		int value = scan.nextInt();
-		System.out.println("Input: " + value);
+		double value = scan.nextDouble();
+		System.out.println("Input: " + (double)value);
 		System.out.println("------");
 		System.out.println("Output");
-		System.out.printf("Square: %.0f%n", Math.pow(value, 2));
-		System.out.printf("cube: %.0f%n", Math.pow(value, 3));
-		System.out.printf("fourth power: %.0f%n", Math.pow(value, 4));
+		System.out.printf("Square: %.2f%n", Math.pow((double)value, 2));
+		System.out.printf("cube: %.2f%n", Math.pow((double)value, 3));
+		System.out.printf("fourth power: %.2f%n", Math.pow((double)value, 4));
 	}
 
 }
