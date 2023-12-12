d94d15decc998431ccf92eefa8b84379bb4d69ee
lentraadmin <lentraadmin@lentraadmin-ThinkPad-E14-Gen-4>
2023-12-12 21:47:52 +0530

    Done

--- src/assignment_practice/MyFirstCode.java
+++ src/assignment_practice/MyFirstCode.java
@@ -1,12 +1,22 @@
 package assignment_practice;
 
-public class MyFirstCode {
-	public static void main(String[] args) {
-	int ln = 1234_23;
-		System.out.println(ln);
-	
-	}
-	
+//public class MyFirstCode {
+//	public static void main(String[] args) {
+//	int ln = 1234_23;
+//		System.out.println(ln);
+//		
+//	
+//	}
+//	
+//
+//}
 
-}
+class Test {
+	  public static void main(String[] args) {
+	      for(int i = 0; 1; i++) {
+	      System.out.println("Hello"); 
+	      break; 
+	      } 
+	    } 
+	}
         
---
+++ src/assignment_practice/ObjectPractice.java
@@ -0,0 +1,115 @@
+package assignment_practice;
+
+public class ObjectPractice {
+
+}
+
+class Vehicles{
+
+	String Brand;
+	String Color;
+	String type;
+
+	void move(String destination) {
+
+	}
+
+}
+
+class Human{
+
+	String name;
+	int age;
+	String color;
+	int height;
+
+	void toTalk(String Content) {
+
+	}
+	void toWalk(double distance) {
+		
+	}
+}
+
+class Mobile{
+	
+	String Brand;
+	String color;
+	double price;
+	String type;
+
+	void toCall(String conversation) {
+		
+	}
+	void toSendMessage(String text) {
+		
+	}
+}
+
+class Fridge{
+	
+	String Brand;
+	String Type;
+	String Color;
+	double Price;
+	
+	void toCool(String FoodItems) {
+		
+	}
+}
+
+class Fan{
+	
+	String Brand;
+	double Price;
+	int Speed;
+	String type;
+	
+	void tooscillate() {
+		
+	}
+	void toturnOn(int Speed) {
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
